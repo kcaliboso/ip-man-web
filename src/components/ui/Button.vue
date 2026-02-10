b49aa06e0ce4cca007f69f49b4fdfa1dc0cf0b50
Kevin Caliboso <calibosokevin@gmail.com>
2026-02-10 11:32:23 +0800

    feat: added reusable button

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -2,7 +2,12 @@
   <button
     :class="
       cn(
-        'rounded bg-slate-900 px-4 py-2 font-medium text-white hover:bg-slate-800 dark:hover:bg-slate-700 cursor-pointer',
+        'rounded text-black dark:text-white cursor-pointer',
+        variant === 'solid' &&
+          ' bg-slate-900 px-4 py-2 font-medium text-white hover:bg-slate-800 dark:hover:bg-slate-700 ',
+        variant === 'link' && ' py-1 font-medium  ',
+        variant === 'outline' &&
+          '  px-3 py-1 font-medium  outline outline-slate-500 dark:hover:bg-slate-700 ',
         className,
       )
     "
@@ -14,8 +19,16 @@
 
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
-withDefaults(defineProps<{ type?: 'submit' | 'button' | 'reset'; className?: string }>(), {
-  className: '',
-  type: 'button',
-})
+withDefaults(
+  defineProps<{
+    variant?: 'solid' | 'link' | 'outline'
+    type?: 'submit' | 'button' | 'reset'
+    className?: string
+  }>(),
+  {
+    className: '',
+    type: 'button',
+    variant: 'solid',
+  },
+)
 </script>
