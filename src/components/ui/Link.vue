af1b04b28b0c0a2fa5c2ec05cc487855ca2b40d5
Kevin Caliboso <calibosokevin@gmail.com>
2026-02-10 09:34:31 +0800

    feat: created reusable RouterLink component (Link)

--- src/components/ui/Link.vue
+++ src/components/ui/Link.vue
@@ -1,13 +1,17 @@
 <template>
-  <RouterLink class="rounded-md hover:bg-slate-200 px-2 py-1" :to="{ name: linkName }">{{
-    linkLabel
-  }}</RouterLink>
+  <RouterLink
+    :class="cn('rounded-md px-2 py-1 hover:bg-slate-200 dark:hover:bg-slate-800', className)"
+    :to="{ name: linkName }"
+    >{{ linkLabel }}</RouterLink
+  >
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { cn } from '@/lib/utils'
 defineProps<{
   linkName: string
   linkLabel: string
+  className?: string
 }>()
 </script>
