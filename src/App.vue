17724f5e8e362e091dfc5e6b492299b6cb96200d
Kevin Caliboso <calibosokevin@gmail.com>
2026-02-10 11:32:40 +0800

    feat: config toaster to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <main class="bg-slate-50 text-black dark:bg-slate-800 dark:text-slate-50">
+    <Toaster theme="system" />
     <RouterView />
   </main>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Toaster } from 'vue-sonner'
+import 'vue-sonner/style.css'
 </script>
