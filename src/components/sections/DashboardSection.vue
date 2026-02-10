4b079a76a0c12d758d91c1dd5c01d48b57288fb3
Kevin Caliboso <calibosokevin@gmail.com>
2026-02-10 15:45:56 +0800

    feat: created reusable dashboard section component

---
+++ src/components/sections/DashboardSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="flex h-[calc(100vh-3rem)] flex-1 flex-col gap-4 overflow-y-auto p-4">
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+//
+</script>
