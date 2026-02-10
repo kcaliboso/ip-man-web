71369b103b90971faf2ff2412013079d0b057946
Kevin Caliboso <calibosokevin@gmail.com>
2026-02-10 11:32:59 +0800

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import './assets/css/main.css'
 
 import App from './App.vue'
+import { pinia } from './stores'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
